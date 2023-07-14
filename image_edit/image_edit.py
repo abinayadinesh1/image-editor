2ae9731f843c3314c5c68e808794c421796f9960
Abinaya Dinesh <abinayadinesh1@gmail.com>
2023-07-14 01:31:18 -0500

    having to pull from os listdir is annoying, next step to add delete img functionality to clean up the view

--- image_edit/image_edit.py
+++ image_edit/image_edit.py
@@ -1,41 +1,104 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 from rxconfig import config
-
+import asyncio
 import reflex as rx
+from typing import List
+import os
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
-
-
+options: List[str] = [
+    "LAB",
+    "HSV",
+    "Greyscale",
+    "LUV",
+    "YUV",
+    "BayerGBRG",
+    "NV21"
+]
+def image_box(filename: str):
+    return rx.center(
+        rx.vstack(
+            rx.image(src=filename, width="120px", height="auto"), #so far hardcoded, in the for each need to add path for image
+            rx.box(rx.text(filename), bg="white"),            
+        ),
+        padding="2em",
+    )
 class State(rx.State):
     """The app state."""
+    is_uploading: bool
+    option: str = "No selection yet."
+    uploaded_images: list
+    img : list[str]
 
-    pass
+    @rx.var
+    def file_list(self) -> str:
+        """Get the names of each file as a list of strings."""
+        self.uploaded_images = os.listdir(rx.get_asset_path())
+        return os.listdir(rx.get_asset_path())
+
+    async def handle_upload(self, files: List[rx.UploadFile]):
+        """Handle the upload of file(s).
+
+        Args:
+            files: The uploaded files.
+        """
+        for file in files:
+            upload_data = await file.read()
+            outfile = rx.get_asset_path(file.filename)
+
+
+            # Save the file.
+            with open(outfile, "wb") as file_object:
+                file_object.write(upload_data)
+            self.img.append(file.filename)
+    
+    async def stop_upload(self):
+        """Stop the file upload."""
+        await asyncio.sleep(1)
+        self.is_uploading = False
 
 
 def index() -> rx.Component:
-    return rx.fragment(
-        rx.color_mode_button(rx.color_mode_icon(), float="right"),
+    return rx.center(
         rx.vstack(
+            rx.color_mode_button(rx.color_mode_icon(), float="right"),
             rx.heading("Welcome to Image Theory!", font_size="2em"),
-            rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
-            rx.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": rx.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
+            rx.text("What does your image look like in another color space?"),
+                rx.responsive_grid(
+                    rx.foreach(State.file_list, image_box),
+                    columns=[2, 4, 6],
+                ),
+            rx.upload(
+                    rx.button(
+                            "Select File(s)",
+                            color="purple",
+                            bg="white",
+                            text_align="center",
+                            border="1px dotted black",
+                            padding="1.0em",
+                            margin_top = "0.5em",
+                            margin_bottom = "0.5em"
+                        ),
+            text_align="center",
             ),
+            rx.button(
+                "Upload",
+                height="70px",
+                width="200px",
+                on_click=lambda: State.handle_upload(rx.upload_files()),
+                padding="1.5em",
+                margin_bottom="2em",
+            ),
+            rx.select(
+                options,
+                placeholder="Select a color space.",
+                value=State.option,
+                on_change=State.set_option, #we havent define this, so does it do anything? update_image
+            ),
+            rx.text(State.option),  
             spacing="1.5em",
             font_size="2em",
             padding_top="10%",
-        ),
+        )
     )
 
 
